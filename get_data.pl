37f07916437ed6d2daefb9af1ba4c7fcd04afc2d
ikleiman <ikleiman@cs.stonybrook.edu>
2018-02-20 20:29:44 -0500

    aesthetics
    can add/view each wallet

--- get_data.pl
+++ get_data.pl
@@ -26,15 +26,18 @@ sub reprocess_add_prices {
 	foreach my $hash (@{$wallets_data}) {
 		my $cmc_usd_price;
 		my $cmc_btc_price;
+		my $cmc_name;
 		# gets the prices of the necessary symbols
 		foreach my $hashes (@{$cmc_data}) {
 			if ($hashes->{symbol} eq $hash->{symbol}) {
 				$cmc_usd_price = $hashes->{price_usd};
 				$cmc_btc_price = $hashes->{price_btc};
+				$cmc_name = $hashes->{name};
 				last;
 			}
 		}
 		# set the USD and BTC price for each coin in each wallet & the whole value of each wallet
+		$hash->{name} = $cmc_name;
 		$hash->{usd_price} = $cmc_usd_price;
 		$hash->{btc_price} = $cmc_btc_price;
 		$hash->{usd_value} = $cmc_usd_price * $hash->{amt};
@@ -55,12 +58,40 @@ sub read_validate_file {
 	return path($path . "/" . $file . ".json")->slurp;
 }
 
+sub reformat_insert_data {
+	my ($data, $q) = @_;
+	$data = decode_json($data);
+	my @wallets_data = @{$data->{wallets}};
+	my %hash = (
+		symbol => uc($q->param("symbol")),
+		amt => $q->param("amt"),
+		location => $q->param("location"),
+	);
+	push(@wallets_data, \%hash);
+	$data->{wallets} = \@wallets_data;
+	return encode_json($data);
+}
+
 sub get_my_info {
 	my ($path, $user) = @_;
 	print encode_json(
 		reprocess_add_prices(
 			read_validate_file($path, $user)
-		));
+	));
+}
+
+sub input_new_info {
+	my ($path, $user, $q) = @_;
+	chmod 0777, $path;
+	chmod 0777, $path . '/' . $user . '.json';
+	#path($path . '/' . $user . '.json')->spew("hi");
+	path($path . '/' . $user . '.json')->spew(
+			reformat_insert_data(
+				read_validate_file($path, $user), $q
+			));
+	chmod 0777, $path;
+	chmod 0777, $path . '/' . $user . '.json';
+	print "updated";
 }
 
 BEGIN {
@@ -78,7 +109,13 @@ BEGIN {
 		}
 	}
 	elsif ($cgi->param("method") eq "post_data") {
-
+		if (defined $cgi->param("user")) {
+			input_new_info($data_dir_name, $cgi->param("user"), $cgi);
+		}
+		else {
+			print "undefined user";
+			exit
+		}
 	}
 	else {
 		print "invalid method\n";
--- js/script.js
+++ js/script.js
@@ -6,7 +6,103 @@
 */
 
 $(document).ready(function() {
-	$.get("get_data.pl", function( data ) {
-		//$( ".result" ).html( data );
+	$.get("get_data.pl?method=get_data&user=shortland", function(data) {
+		var DECIMALS_TO_ROUND = 4;
+		var LONGER_DECIMALS_TO_ROUND = 12;
+
+		var obj = JSON.parse(data);
+		var total_value = obj['usd_value'];
+		var total_value_btc = obj['btc_value'];
+
+		$("#add_content").click(function() {
+			if ($("#add_content").html() == "+") {
+				$("#add_content").html("-");
+				$("#add_content").css({"background-color" : "rgba(255, 255, 255, 1.0)", "color" : "rgba(10, 86, 208, 1.0)"});
+				$("#adder").show();
+				$("#adder").animate({"height" : "150px"}, 800);
+			}
+			else {
+				$("#add_content").html("+");
+				$("#add_content").css({"color" : "rgba(255, 255, 255, 1.0)", "background-color" : "rgba(10, 86, 208, 1.0)"});
+				$("#adder").animate({"height" : "0px"}, 800, function() {
+					$("#adder").hide();
+				});
+			}
+		});
+
+		$("#content").append(
+			"<div class='object' style='height:130px;padding-left:20px;box-sizing: border-box;'>" +
+				"<h3 style='text-decoration:underline'>Balance</h3>" +
+				"<p>&nbsp;&nbsp;&nbsp;&nbsp;USD: $" + total_value + "</p>" + 
+				"<p>&nbsp;&nbsp;&nbsp;&nbsp;BTC: " + total_value_btc + "</p>" + 
+			"</div>"
+		);
+
+		$("#content").append(`
+			<div id='adder' style='display:none;height:0px;'>
+			</br>
+				<center>
+					<table border='0px'>
+						<tr>
+							<td>Symbol:</td>
+							<td><input class='textbox' id='symbol_value' type='text' placeholder=' BTC'/></td>
+						</tr>
+						<tr>
+							<td>Location:</td>
+							<td><input class='textbox' id='location_value' type='text' placeholder=' Coinbase'/></td>
+						</tr>
+						<tr>
+							<td>Amount:</td>
+							<td><input class='textbox' id='amt_value' type='text' placeholder=' 0.08324'/></td>
+						</tr>
+						<tr>
+							<td tdspan='2'><button type='button' id='save_adder'>Save</button></td>
+						</tr>
+					</table>
+				</center>
+			</div>
+		`);
+
+		for (var i = 0; i < obj['wallets'].length; i++) {
+			$("#content").append(
+				"<div class='object'>" +
+					"<table class='tblof' style='height:100%;overflow:scroll;'>" +
+						"<tr>" +
+							"<td rowspan='4' align='center' style='width:100px;height:100%;vertical-align:middle;'>" +
+								"<div style='display:inline-block;horizontal-align:center;width:100px !important;'>" +
+									"<img src='color/" + obj['wallets'][i]['symbol'].toLowerCase() + ".png' class='sym_pic' />" +
+								"</div>" +
+							"</td>" + 
+							"<td><div class='wide150'>Name: " + obj['wallets'][i]['name'] + "</div></td>" +
+							"<td style='min-width:250px;'>Price <small>(USD)</small>: $" + (obj['wallets'][i]['usd_price']) + "</td>" +
+							
+						"</tr>" + 
+						"<tr>" +
+							// sym.img
+							"<td><div class='wide150'>Symbol: " + obj['wallets'][i]['symbol'] + "</div></td>" +
+							"<td style='min-width:250px;'>Price <small>(BTC)</small>: " + (obj['wallets'][i]['btc_price']) + "</td>" +
+						"</tr>" + 
+						"<tr>" +
+							// sym.img
+							"<td><div class='wide150'>Wallet: " + obj['wallets'][i]['location'] + "</div></td>" +
+							"<td style='min-width:250px;'>Value <small>(USD)</small>: $" + (obj['wallets'][i]['usd_value']) + "</td>" +
+						"</tr>" + 
+						"<tr>" +
+							// sym.img
+							"<td><div class='wide150'>Amount: " + parseFloat(obj['wallets'][i]['amt']).toFixed(DECIMALS_TO_ROUND) + "</div></td>" +
+							"<td style='min-width:250px;'>Value <small>(BTC)</small>: " + (obj['wallets'][i]['btc_value']) + "</td>" +
+						"</tr>" + 
+					"</table>" +
+				"</div>"
+			);
+		}
+
+		$(".object").css({"width" : $(window).width()});
+		
+		$("#save_adder").click(function() {
+			$.post("get_data.pl", {method: "post_data", user: "shortland", symbol: $("#symbol_value").val(), amt: $("#amt_value").val(), location: $("#location_value").val()}, function(data) {
+				alert(data);
+			});
+		});
 	});
 });
